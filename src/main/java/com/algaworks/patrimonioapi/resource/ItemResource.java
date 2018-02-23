e030545042e79ecd2bd37691e8b7a309364378e0
Karen Vicente <karenyasmin37@gmail.com>
2018-02-23 18:21:51 -0300

    testando cross origin

--- src/main/java/com/algaworks/patrimonioapi/resource/ItemResource.java
+++ src/main/java/com/algaworks/patrimonioapi/resource/ItemResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import com.algaworks.patrimonioapi.repository.ItemRepository;
  * @author Karen 21 de fev de 2018
  */
 @RestController
+//@CrossOrigin("${origem-permitida}")
 public class ItemResource {
 
 	@Autowired
